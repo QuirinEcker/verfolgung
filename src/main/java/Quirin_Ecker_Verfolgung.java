8ca475430e008c4d9d22a484b379a4fee9833cbf
QuirinEcker <quirin.ecker@bajupa.com>
2017-10-26 13:37:52 +0200

    initial commit

---
+++ src/main/java/Quirin_Ecker_Verfolgung.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Quirin_Ecker_Verfolgung {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        double geschwindigkeit01;
+        double geschwindigkeit02;
+        double verspaetung;
+        double wiedersehen;
+        double stunden;
+        double minuten;
+
+
+        System.out.print("Geschwindigkeit der Gruppe 1: ");
+        geschwindigkeit01 = scanner.nextDouble();
+
+        System.out.print("Geschwindigkeit der Gruppe 2: ");
+        geschwindigkeit02 = scanner.nextDouble();
+
+        System.out.print("Verspätung der Gruppe 2: ");
+        verspaetung = scanner.nextDouble();
+
+
+
+
+
+
+    }
+}
